899bc765e65041b0808b27e0c013f858207ab86a
Eldolfin <oscar.le-dauphin@epita.fr>
2023-06-19 03:54:20 +0200

    update

--- configuration.nix
+++ configuration.nix
@@ -4,21 +4,48 @@
 
 { config, pkgs, ... }:
 
+let
+  unstableTarball =
+    fetchTarball
+      "https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz";
+in
 {
   imports =
     [
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      # GPU Passthrough
+      ./gpu-passthrough.nix
     ];
 
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # enable cuda
+  nixpkgs.config.cudaSupport = true;
+
   # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/nvme0n1";
-  boot.loader.grub.useOSProber = true;
+
+  # grub
+  # boot.loader.grub.enable = true;
+  # boot.loader.grub.device = "/dev/nvme0n1";
+  # boot.loader.grub.useOSProber = true;
+
+  # systemd
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot";
+    };
+  };
+
+  #  boot.initrd.luks.devices = {
+  #    name = "nixos";
+  #    device = "/dev/disk/by-uuid/9ACA-8EC6";
+  #    preLVM = true;
+  #  };
 
   networking.hostName = "nixos-tour"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -26,6 +53,7 @@
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -65,35 +93,32 @@
       sessionCommands = ''
         ${pkgs.xorg.xrdb}/bin/xrdb -merge <${pkgs.writeText "Xresources" ''
           Xcursor.theme: Adwaita
-          Xcursor.size: 64
-        ''}
+            ''}
       '';
     };
 
     windowManager.i3 = {
       enable = true;
-      package = pkgs.i3-gaps;
-      extraPackages = with pkgs; [
-        rofi
-        i3status
-        i3lock
-      ];
+      package = pkgs.i3;
+      #      extraPackages = with pkgs; [
+      #      ];
     };
 
     videoDrivers = [ "nvidia" ];
   };
 
+  # fix for i3blocks
+  environment.pathsToLink = [ "/libexec" ];
 
-  hardware.opengl.enable = true;
 
   # Optionally, you may need to select the appropriate driver version for your specific GPU.
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+  # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.oscar = {
     isNormalUser = true;
     description = "Oscar Le Dauphin";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" "docker" "i2c" "libvirtd" ];
     /* packages = with pkgs; [ ]; */
   };
 
@@ -107,8 +132,8 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    neovim
-    i3-gaps
+    /* neovim */
+    # unstable.helix
     git
     exa
     fd
@@ -130,7 +155,6 @@
     lazygit
     feh
     flameshot
-    clipit
     emote
     bat
     gnumake
@@ -146,7 +170,76 @@
     thunderbird
     redshift
     killall
+    cmake
+    clang
+    clang-tools
+    ddccontrol
+    ddcutil
+    ddcui
+    rust-analyzer
+    usbutils
+    android-tools
+    python311
+    python311Packages.pip
+    qbittorrent
+    ripgrep
+    libreoffice
+    nix-index
+    tailscale
+    virt-manager
+    dnsmasq
+    python310.pkgs.psutil
+    mullvad-vpn
+
+    # steam fix ?? 
+    pango
+    harfbuzz
+    libthai
+
   ];
+
+  # enable mullvad-vpn
+  services.mullvad-vpn.enable = true;
+
+  # polkit
+  security.polkit.enable = true;
+
+  # hyprland (wm)
+  # programs.hyprland.enable = true;
+
+  # external display brightness control requires
+  # i2c to be enabled
+  boot.kernelModules = [ "i2c-dev" ];
+  services.udev.extraRules = ''
+    KERNEL=="i2c-[0-9]*", GROUP="i2c", MODE="0660"
+  '';
+
+  # tailscale
+  services.tailscale.enable = true;
+
+  # virt-manager
+  virtualisation.libvirtd = {
+    enable = true;
+    onBoot = "ignore";
+    onShutdown = "shutdown";
+    qemu.ovmf.enable = true;
+    qemu.runAsRoot = true;
+  };
+  programs.dconf.enable = true;
+
+  # something something qemu single gpu passthrough blah blah blah
+  systemd.services.libvirtd.preStart = ''
+    mkdir -p /var/lib/libvirt
+    # mkdir -p /var/lib/libvirt/vgabios
+
+
+    rm -r /var/lib/libvirt/hooks
+    ln -sf /etc/nixos/git-repo/hooks/ /var/lib/libvirt/
+    # ln -sf /etc/nixos/git-repo/patched.rom /var/lib/libvirt/vgabios/patched.rom
+
+    chmod -R +x /var/lib/libvirt/hooks/
+    # chmod +x /var/lib/libvirt/hooks/kvm.conf
+  '';
 
   # bluetooth/audio setup
   sound.enable = true;
@@ -170,9 +263,6 @@
   # fix for steam
   hardware.opengl.driSupport32Bit = true;
 
-  # Spice vda for shared clipboard in vm
-  services.spice-vdagentd.enable = true;
-
   # zsh as default shell
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
@@ -182,18 +272,34 @@
     (nerdfonts.override { fonts = [ "UbuntuMono" ]; })
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  # docker setup
+  virtualisation.docker.enable = true;
+  virtualisation.docker.rootless = {
+    enable = true;
+    setSocketVariable = true;
+  };
 
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
+
+  # Enable syncthing daemon
+  services =
+    {
+      syncthing = {
+        enable = true;
+        user = "oscar";
+      };
+    };
+
+  # enable flakes
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
