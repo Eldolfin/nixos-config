b1a2701d52ce6e71fdd488fd23f767f37ecccc7a
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2023-11-12 20:26:28 +0100

    change laptop dpi

--- configuration-laptop.nix
+++ configuration-laptop.nix
@@ -5,69 +5,69 @@
 { config, pkgs, ... }:
 
 {
-    imports =
-        [
-        <nixos-hardware/dell/xps/15-9560/nvidia>
-            /etc/nixos/git-repo/common.nix
-        ];
+  imports =
+    [
+      <nixos-hardware/dell/xps/15-9560/nvidia>
+      /etc/nixos/git-repo/common.nix
+    ];
 
 
-# Wifi EPITA
-   # networking = {
-   #     interfaces = {
-   #         wlp59s0.ipv4.addresses = [{
-   #             prefixLength = 64;
-   #         }];
-   #     };
-   #     wireless.networks.IONIS.auth = ''
-   #         eap=PEAP
-   #         identity="oscar.le-dauphin@epita.fr"
-   #         password="vnzpeBAL"
-   #         '';
-   # };
+  # Wifi EPITA
+  # networking = {
+  #     interfaces = {
+  #         wlp59s0.ipv4.addresses = [{
+  #             prefixLength = 64;
+  #         }];
+  #     };
+  #     wireless.networks.IONIS.auth = ''
+  #         eap=PEAP
+  #         identity="oscar.le-dauphin@epita.fr"
+  #         password="vnzpeBAL"
+  #         '';
+  # };
 
-    networking.hostName = "nixos-portable"; # Define your hostname.
-        services.xserver.layout = "us";
-# for firefox to support touchscreen scroll
-    environment.sessionVariables = {
-        MOZ_USE_XINPUT2 = "1";
-    };
+  networking.hostName = "nixos-portable"; # Define your hostname.
+  services.xserver.layout = "us";
+  # for firefox to support touchscreen scroll
+  environment.sessionVariables = {
+    MOZ_USE_XINPUT2 = "1";
+  };
 
 
-# Optionally, you may need to select the appropriate driver version for your specific GPU.
-# hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
-    hardware.nvidia.prime = {
+  # Optionally, you may need to select the appropriate driver version for your specific GPU.
+  # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+  hardware.nvidia.prime = {
 
-# Make sure to use the correct Bus ID values for your system!
-        intelBusId = "PCI:0:2:0";
-        nvidiaBusId = "PCI:1:0:0";
-    };
+    # Make sure to use the correct Bus ID values for your system!
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0";
+  };
 
-    services.illum.enable = true;
+  services.illum.enable = true;
 
-# lol (800MHz)
-# powerManagement.cpufreq.max = 800;
+  # lol (800MHz)
+  # powerManagement.cpufreq.max = 800;
 
-# Enable the OpenSSH daemon.
-    services.openssh.enable = false;
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = false;
 
-# touchscreen maybe
-    services.xserver.synaptics = {
-        enable = true;
-        twoFingerScroll = true;
-        maxSpeed = "4.0";
-        scrollDelta = -75;
-    };
+  # touchscreen maybe
+  services.xserver.synaptics = {
+    enable = true;
+    twoFingerScroll = true;
+    maxSpeed = "4.0";
+    scrollDelta = -75;
+  };
 
-    services.xserver.dpi = 400;
+  services.xserver.dpi = 200;
 
-    services.xserver.libinput.enable = false;
+  services.xserver.libinput.enable = false;
 
-# This value determines the NixOS release from which the default
-# settings for stateful data, like file locations and database versions
-# on your system were taken. It‘s perfectly fine and recommended to leave
-# this value at the release version of the first install of this system.
-# Before changing this value read the documentation for this option
-# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-    system.stateVersion = "22.11"; # Did you read the comment?
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.11"; # Did you read the comment?
 }
