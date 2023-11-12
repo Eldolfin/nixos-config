6fa1e18ee394f1de86db48c3a2aeb37bf44ad08e
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2023-11-12 13:20:15 +0100

    updae

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [
       # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      /etc/nixos/hardware-configuration.nix
       # GPU Passthrough
       # ./gpu-passthrough.nix
 
@@ -21,6 +21,22 @@
         })
       )
     ];
+
+# pin docker to older nixpkgs: https://github.com/NixOS/nixpkgs/issues/244159
+# TODO: remove when fixed
+  nixpkgs.overlays = [
+    (let
+      pinnedPkgs = import(pkgs.fetchFromGitHub {
+        owner = "NixOS";
+        repo = "nixpkgs";
+        rev = "b6bbc53029a31f788ffed9ea2d459f0bb0f0fbfc";
+        sha256 = "sha256-JVFoTY3rs1uDHbh0llRb1BcTNx26fGSLSiPmjojT+KY=";
+      }) {};
+    in
+    final: prev: {
+      docker = pinnedPkgs.docker;
+    })
+  ];
 
 
   # Allow unfree packages
@@ -163,7 +179,7 @@
   users.users.oscar = {
     isNormalUser = true;
     description = "Oscar Le Dauphin";
-    extraGroups = [ "networkmanager" "wheel" "docker" "i2c" "libvirtd" ];
+    extraGroups = [ "networkmanager" "wheel" "docker" "i2c" "libvirtd" "input"];
     hashedPassword = "$y$j9T$CLXLAGMu18fDGm90VWDY0/$/K9714xLsq2iIaC1taF/AanvyL0PGNpgiyHDcXFKRr6";
     /* packages = with pkgs; [ ]; */
   };
