e0b5bd1e23a9df177cabcdd3fa6a60ec0b2550c7
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-04-24 16:35:53 +0200

    System switch

--- services/sunshine.nix
+++ services/sunshine.nix
@@ -9,4 +9,20 @@
     };
     services.avahi.publish.enable = true;
     services.avahi.publish.userServices = true;
+    security.wrappers.sunshine = {
+        owner = "root";
+        group = "root";
+        capabilities = "cap_sys_admin+p";
+        source = "${pkgs.sunshine}/bin/sunshine";
+    };
+    systemd.user.services.sunshine = {
+        description = "Sunshine self-hosted game stream host for Moonlight";
+        startLimitBurst = 5;
+        startLimitIntervalSec = 500;
+        serviceConfig = {
+            ExecStart = "${config.security.wrapperDir}/sunshine";
+            Restart = "on-failure";
+            RestartSec = "5s";
+        };
+    };
 }
