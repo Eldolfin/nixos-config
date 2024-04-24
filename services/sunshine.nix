ceb3faa591fb242890ec29d62c484340fd13e843
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-04-24 16:32:45 +0200

    System switch

--- services/sunshine.nix
+++ services/sunshine.nix
@@ -1,11 +1,11 @@
 {
     networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [ 47984 47989 47990 48010 ];
-    allowedUDPPortRanges = [
-        { from = 47998; to = 48000; }
-        { from = 8000; to = 8010; }
-    ];
+        enable = true;
+        allowedTCPPorts = [ 47984 47989 47990 48010 ];
+        allowedUDPPortRanges = [
+            { from = 47998; to = 48000; }
+            { from = 8000; to = 8010; }
+        ];
     };
     services.avahi.publish.enable = true;
     services.avahi.publish.userServices = true;
