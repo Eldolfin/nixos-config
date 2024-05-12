b1d72ec6f4a306ec84121af5b76b58ea81a1b83d
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-05-12 18:08:14 +0200

    System switch

--- common.nix
+++ common.nix
@@ -88,6 +88,12 @@
     noisetorch.enable = true;
     dconf.enable = true;
     nix-ld.enable = true;
+    nh = {
+        enable = true;
+        clean.enable = true;
+        clean.extraArgs = "--keep-since 4d --keep 3";
+        flake = "/etc/nixos/";
+    };
   };
 
   # avahi for sunshine to work
