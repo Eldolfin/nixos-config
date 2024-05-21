fb7494805a913d9a67abc1d9353bd0f67ddffc9a
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-05-22 01:24:02 +0200

    go back to complicated flake :)

--- flake.nix
+++ flake.nix
@@ -1,46 +1,35 @@
 {
   description = "Nixos system configuration";
 
-#  inputs = {
-#    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-#    # home-manager.url = "github:nix-community/home-manager";
-#    # home-manager.inputs.nixpkgs.follows = "nixpkgs";
-#
-#    stylix.url = "github:danth/stylix";
-#  };
-#
-#  outputs = { self, nixpkgs, stylix, ... }:
-#    let
-#      system = "x86_64-linux";
-#    in
-#    {
-#      nixosConfigurations."oscar-portable" = nixpkgs.lib.nixosSystem {
-#        inherit system;
-#        modules = [
-#            stylix.nixosModules.stylix
-#          ./hosts/configuration-laptop.nix
-#          ./hosts/hardware-configuration-laptop.nix
-#        ];
-#      };
-#
-#      nixosConfigurations."oscar-tour" = nixpkgs.lib.nixosSystem {
-#        inherit system;
-#        modules = [
-#            stylix.nixosModules.stylix
-#          ./hosts/configuration-tour.nix
-#          ./hosts/hardware-configuration-tour.nix
-#        ];
-#      };
-#    };
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # home-manager.url = "github:nix-community/home-manager";
+    # home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
     stylix.url = "github:danth/stylix";
   };
 
-  outputs = { nixpkgs, stylix, ... }: {
-    nixosConfigurations."oscar-portable" = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, stylix, ... }:
+    let
       system = "x86_64-linux";
-    modules = [ stylix.nixosModules.stylix ./hosts/configuration-laptop.nix ./hosts/hardware-configuration-laptop.nix ];
+    in
+    {
+      nixosConfigurations."oscar-portable" = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+            stylix.nixosModules.stylix
+          ./hosts/configuration-laptop.nix
+          ./hosts/hardware-configuration-laptop.nix
+        ];
+      };
+
+      nixosConfigurations."oscar-tour" = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+            stylix.nixosModules.stylix
+          ./hosts/configuration-tour.nix
+          ./hosts/hardware-configuration-tour.nix
+        ];
+      };
     };
-  };
 }
