d9ea5f583f2eb0bbfcb92400b4713a129908c382
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-05-22 01:49:29 +0200

    System switch

--- flake.nix
+++ flake.nix
@@ -3,21 +3,30 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    # home-manager.url = "github:nix-community/home-manager";
-    # home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     stylix.url = "github:danth/stylix";
   };
 
-  outputs = { self, nixpkgs, stylix, ... }:
+  outputs = { self, nixpkgs, stylix, home-manager, ... }:
     let
       system = "x86_64-linux";
+    commonModules = [
+        stylix.nixosModules.stylix
+      ./common.nix
+          home-manager.nixosModules.home-manager
+          {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.oscar = import ./home-manager/home.nix;
+          }
+      ];
     in
     {
       nixosConfigurations."oscar-portable" = nixpkgs.lib.nixosSystem {
         inherit system;
-        modules = [
-            stylix.nixosModules.stylix
+        modules = commonModules ++ [
           ./hosts/configuration-laptop.nix
           ./hosts/hardware-configuration-laptop.nix
         ];
@@ -25,8 +34,7 @@
 
       nixosConfigurations."oscar-tour" = nixpkgs.lib.nixosSystem {
         inherit system;
-        modules = [
-            stylix.nixosModules.stylix
+        modules = commonModules ++ [
           ./hosts/configuration-tour.nix
           ./hosts/hardware-configuration-tour.nix
         ];
