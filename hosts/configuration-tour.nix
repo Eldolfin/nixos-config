2dcedda516f501cdb9395f78c0e191ac22cbc751
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-06-10 20:10:06 +0200

    System switch

--- hosts/configuration-tour.nix
+++ hosts/configuration-tour.nix
@@ -23,7 +23,7 @@
     modesetting.enable = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
-    open = true;
+    # open = true;
   };
 
   # Enable the OpenSSH daemon.
