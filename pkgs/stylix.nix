7f47efc9f3ddd171091ebc1a488ab00016500bc4
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-06-12 11:21:36 +0200

    System switch

--- pkgs/stylix.nix
+++ pkgs/stylix.nix
@@ -2,7 +2,7 @@
 {
   stylix = {
     image = ./images/background.jpg;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
     autoEnable = true;
     opacity.terminal = 0.9;
     polarity = "dark";
