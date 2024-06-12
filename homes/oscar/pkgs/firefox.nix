10ef048b3eff1b360b853987bb82fa8a5f797e8e
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-06-12 07:43:17 +0200

    System switch

--- homes/oscar/pkgs/firefox.nix
+++ homes/oscar/pkgs/firefox.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 let
   nur-no-pkgs = import (builtins.fetchTarball {
-    url = "https://github.com/nix-community/NUR/archive/48bac29969e0e9508f6a203f001fe606960cbacd.zip";
+    url = "https://github.com/nix-community/NUR/archive/48bac29969e0e9508f6a203f001fe606960cbacd.tar.gz";
     sha256 = "0yckdj3sj1wds0hxmbq7mhsch463jd05drairyvis52gv18a0bv7";
   }) { };
 in
