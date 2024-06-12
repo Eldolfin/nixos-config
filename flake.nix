7ec85bfea85b6d2828e860c756de6ff90873645e
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-06-12 15:17:58 +0200

    System switch

--- flake.nix
+++ flake.nix
@@ -27,6 +27,8 @@
         ./common.nix
         home-manager.nixosModules.home-manager
         {
+
+          home-manager.backupFileExtension = "bak";
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.oscar = import ./homes/oscar/home.nix;
