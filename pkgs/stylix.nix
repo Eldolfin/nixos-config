23b8bef9f0e3a4dee0007d6d6a2c8c61f1da81fb
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-06-12 09:43:23 +0200

    System switch

--- pkgs/stylix.nix
+++ pkgs/stylix.nix
@@ -1,34 +1,37 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   stylix = {
     image = ./images/background.jpg;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
     autoEnable = true;
     opacity.terminal = 0.9;
     polarity = "dark";
-    targets = { gnome.enable = true; };
+    targets = {
+      gnome.enable = true;
+    };
 
     fonts = {
       serif = {
-        package = pkgs.dejavu_fonts;
-        name = "DejaVu Serif";
+        package = pkgs.meslo-lgs-nf;
+        name = "UbuntuMono Nerd Font";
       };
 
       sansSerif = {
-        package = pkgs.dejavu_fonts;
-        name = "DejaVu Sans";
+        package = pkgs.meslo-lgs-nf;
+        name = "meslo-lgs-nf";
       };
 
       monospace = {
-        package = pkgs.dejavu_fonts;
-        name = "DejaVu Sans Mono";
+        package = pkgs.meslo-lgs-nf;
+        name = "meslo-lgs-nf";
       };
 
       emoji = {
-        package = pkgs.noto-fonts-emoji;
-        name = "Noto Color Emoji";
+        package = pkgs.meslo-lgs-nf;
+        name = "meslo-lgs-nf";
       };
 
-      sizes.terminal = 16;
+      sizes.terminal = 20;
     };
   };
 }
