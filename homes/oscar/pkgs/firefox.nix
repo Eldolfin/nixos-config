273a90d071906f61b07e4d53170159408dbf6663
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-06-12 07:02:11 +0200

    System switch

--- homes/oscar/pkgs/firefox.nix
+++ homes/oscar/pkgs/firefox.nix
@@ -2,5 +2,17 @@
 {
   programs.firefox = {
     package = pkgs.librewolf;
+    profiles.homemanager = {
+      bookmarks = [
+        {
+          name = "Dashboard";
+          url = "http://192.168.1.1:7575";
+        }
+        {
+          name = "YT";
+          url = "https://invidious.eldolfin.top/feed/subscriptions";
+        }
+      ];
+    };
   };
 }
