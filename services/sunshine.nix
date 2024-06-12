17552572522d4b96fe6dcfefa8c7cda1f33ad244
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-06-12 15:03:42 +0200

    System switch

--- services/sunshine.nix
+++ services/sunshine.nix
@@ -1,29 +1,30 @@
-{config, pkgs, ... }:
+{ config, pkgs, ... }:
 {
-    networking.firewall = {
-        enable = true;
-        allowedTCPPorts = [ 47984 47989 47990 48010 ];
-        allowedUDPPortRanges = [
-            { from = 47998; to = 48000; }
-            { from = 8000; to = 8010; }
-        ];
+  # networking.firewall = {
+  #     enable = true;
+  #     allowedTCPPorts = [ 47984 47989 47990 48010 ];
+  #     allowedUDPPortRanges = [
+  #         { from = 47998; to = 48000; }
+  #         { from = 8000; to = 8010; }
+  #     ];
+  # };
+  services.avahi.publish.enable = true;
+  services.avahi.publish.userServices = true;
+  security.wrappers.sunshine = {
+    owner = "root";
+    group = "root";
+    capabilities = "cap_sys_admin+p";
+    source = "${pkgs.sunshine}/bin/sunshine";
+  };
+  systemd.user.services.sunshine = {
+    openFirewall = true;
+    description = "Sunshine self-hosted game stream host for Moonlight";
+    startLimitBurst = 5;
+    startLimitIntervalSec = 500;
+    serviceConfig = {
+      ExecStart = "${config.security.wrapperDir}/sunshine";
+      Restart = "on-failure";
+      RestartSec = "5s";
     };
-    services.avahi.publish.enable = true;
-    services.avahi.publish.userServices = true;
-    security.wrappers.sunshine = {
-        owner = "root";
-        group = "root";
-        capabilities = "cap_sys_admin+p";
-        source = "${pkgs.sunshine}/bin/sunshine";
-    };
-    systemd.user.services.sunshine = {
-        description = "Sunshine self-hosted game stream host for Moonlight";
-        startLimitBurst = 5;
-        startLimitIntervalSec = 500;
-        serviceConfig = {
-            ExecStart = "${config.security.wrapperDir}/sunshine";
-            Restart = "on-failure";
-            RestartSec = "5s";
-        };
-    };
+  };
 }
