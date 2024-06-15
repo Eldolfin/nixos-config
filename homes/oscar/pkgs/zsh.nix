3d1701fc3f262a9d31041c2f4e59ac74020dc57c
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-06-15 19:09:16 +0200

    System switch

--- homes/oscar/pkgs/zsh.nix
+++ homes/oscar/pkgs/zsh.nix
@@ -65,18 +65,14 @@
     };
 
     initExtra = ''
-      fancy-ctrl-z () {
-        if [[ $#BUFFER -eq 0 ]]; then
-          bg
-          zle redisplay
-        else
-          zle push-input
-        fi
-      }
-      zle -N fancy-ctrl-z
       bindkey '^Z' fancy-ctrl-z 
       bindkey "^[[1;5C" forward-word
       bindkey "^[[1;5D" backward-word
     '';
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "fancy-ctrl-z" ];
+    };
   };
 }
