6b751b90fea689df1d73ee73404bed85e9d079d3
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-06-18 00:45:36 +0200

    System switch

--- homes/oscar/pkgs/wm.nix
+++ homes/oscar/pkgs/wm.nix
@@ -12,30 +12,37 @@
           command = "date +%r";
           interval = 1;
         };
-        # Make sure this block comes after the time block
+        # lib.hm.dag.entryBefore
+        # Makes sure this block comes after the time block
+        # (the order is from right to left)
         date = lib.hm.dag.entryBefore [ "time" ] {
           command = "date +'%a %d/%m/%Y'";
           interval = 60;
         };
-        # And this block after the example block
         volume = lib.hm.dag.entryBefore [ "date" ] {
           command = "~/.config/i3/scripts/i3-volume/volume output i3blocks";
           interval = "once";
           signal = 10;
-          label = "VOL";
+          label = "VOL  ";
           markup = "pango";
         };
         cpu = lib.hm.dag.entryBefore [ "volume" ] {
           command = "~/.config/i3blocks/scripts/cpu_usage";
           interval = "10";
-          label = "CPU";
+          label = "CPU  ";
           min_width = "100.00%";
         };
         memory = lib.hm.dag.entryBefore [ "cpu" ] {
           command = "~/.config/i3/scripts/i3memory";
           interval = "5";
-          label = "MEM";
+          label = "MEM  ";
         };
+        disk = lib.hm.dag.entryBefore [ "memory" ] {
+          command = "~/.config/i3/scripts/i3disk";
+          interval = "10";
+          label = "HOME  ";
+        };
+
       };
     };
   };
