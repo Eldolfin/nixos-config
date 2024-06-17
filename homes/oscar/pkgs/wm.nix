6bbfb024e6f498b0fbc260a04b9968b159fcbf71
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-06-18 00:16:48 +0200

    System switch

--- homes/oscar/pkgs/wm.nix
+++ homes/oscar/pkgs/wm.nix
@@ -3,14 +3,15 @@
 {
   programs.rofi = {
     enable = true;
-
   };
-  programs.i3lock.enable = true;
   programs.i3blocks.enable = true;
   programs.swaylock.enable = true;
 
+  xsession.numlock.enable = true;
+
   home.packages = with pkgs; [
     # rofi-bluetooth # useless
+    i3lock-fancy-rapid
     rofi-power-menu
     slock
     picom
