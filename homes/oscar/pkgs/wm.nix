bdbb778cf420f9b7932ea2dfac268c05a6e4f87c
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-06-18 00:30:59 +0200

    System switch

--- homes/oscar/pkgs/wm.nix
+++ homes/oscar/pkgs/wm.nix
@@ -1,10 +1,43 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   programs.rofi = {
     enable = true;
   };
-  programs.i3blocks.enable = true;
+  programs.i3blocks = {
+    enable = true;
+    bars = {
+      bottom = {
+        time = {
+          command = "date +%r";
+          interval = 1;
+        };
+        # Make sure this block comes after the time block
+        date = lib.hm.dag.entryAfter [ "time" ] {
+          command = "date +'%a %d/%m/%Y'";
+          interval = 60;
+        };
+        # And this block after the example block
+        volume = lib.hm.dag.entryAfter [ "date" ] {
+          command = "~/.config/i3/scripts/i3-volume/volume output i3blocks";
+          interval = "once";
+          signal = 10;
+          label = "VOL";
+        };
+        cpu = lib.hm.dag.entryAfter [ "volume" ] {
+          command = "~/.config/i3blocks/scripts/cpu_usage";
+          interval = "10";
+          label = "CPU";
+          min_width = "100.00%";
+        };
+        memory = lib.hm.dag.entryAfter [ "cpu" ] {
+          command = "~/.config/i3/scripts/i3memory";
+          interval = "5";
+          label = "MEM";
+        };
+      };
+    };
+  };
   programs.swaylock.enable = true;
 
   xsession.numlock.enable = true;
