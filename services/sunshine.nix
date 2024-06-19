0534f0d5eaf909ae6418836ad4dbc5ecdf0fd0de
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-06-19 14:42:02 +0200

    System switch

--- services/sunshine.nix
+++ services/sunshine.nix
@@ -1,49 +1,11 @@
 { config, pkgs, ... }:
 {
-  networking.firewall = {
-    enable = false;
-    allowedTCPPorts = [
-      47984
-      47989
-      47990
-      48010
-    ];
-    allowedUDPPortRanges = [
-      {
-        from = 47998;
-        to = 48000;
-      }
-      {
-        from = 8000;
-        to = 8010;
-      }
-    ];
-  };
   services.sunshine = {
     enable = true;
     openFirewall = true;
     capSysAdmin = true;
   };
   systemd.user.services.sunshine = {
-    # description = "Sunshine self-hosted game stream host for Moonlight";
-    startLimitBurst = 5;
-    startLimitIntervalSec = 500;
     wantedBy = [ "multi-user.target" ];
-
-    serviceConfig = {
-      # ExecStart = "${config.security.wrapperDir}/sunshine";
-      Restart = "on-failure";
-      RestartSec = "5s";
-    };
   };
-
-  services.avahi.publish.enable = true;
-  services.avahi.publish.userServices = true;
-  # not sure this is needed
-  # security.wrappers.sunshine = {
-  #   owner = "root";
-  #   group = "root";
-  #   capabilities = "cap_sys_admin+p";
-  #   source = "${pkgs.sunshine}/bin/sunshine";
-  # };
 }
