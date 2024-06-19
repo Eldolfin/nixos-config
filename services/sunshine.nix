7801d0b5af6e57193783e4618555854d9b9670ac
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-06-19 14:48:55 +0200

    System switch

--- services/sunshine.nix
+++ services/sunshine.nix
@@ -4,6 +4,24 @@
     enable = true;
     openFirewall = true;
     # capSysAdmin = true;
+    applications = {
+      env = {
+        PATH = "$(PATH):$(HOME)/.local/bin";
+      };
+      apps = [
+        {
+          name = "1080p Desktop";
+          prep-cmd = [
+            {
+              do = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.DVI-D-0.mode.1920x1080@60";
+              undo = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.DVI-D-0.mode.1920x1200@60";
+            }
+          ];
+          exclude-global-prep-cmd = "false";
+          auto-detach = "true";
+        }
+      ];
+    };
   };
   systemd.user.services.sunshine = {
     wantedBy = [ "multi-user.target" ];
