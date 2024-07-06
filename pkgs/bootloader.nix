a6187fb1001fab3984ea68e766887a81a809563c
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-07-06 20:19:38 +0200

    System switch

--- pkgs/bootloader.nix
+++ pkgs/bootloader.nix
@@ -1,22 +1,22 @@
 {
   boot.loader = {
-    # systemd-boot.enable = true;
+    systemd-boot.enable = true;
 
-    grub = {
-      enable = true;
-      useOSProber = true;
-      efiSupport = true;
-      efiInstallAsRemovable = true; # Otherwise /boot/EFI/BOOT/BOOTX64.EFI isn't generated
-      devices = [ "nodev" ];
-      extraEntriesBeforeNixOS = false;
-      extraEntries = ''
-        menuentry "Reboot" {
-          reboot
-        }
-        menuentry "Poweroff" {
-          halt
-        }
-      '';
-    };
+    # grub = {
+    #   enable = true;
+    #   useOSProber = true;
+    #   efiSupport = true;
+    #   efiInstallAsRemovable = true; # Otherwise /boot/EFI/BOOT/BOOTX64.EFI isn't generated
+    #   devices = [ "nodev" ];
+    #   extraEntriesBeforeNixOS = false;
+    #   extraEntries = ''
+    #     menuentry "Reboot" {
+    #       reboot
+    #     }
+    #     menuentry "Poweroff" {
+    #       halt
+    #     }
+    #   '';
+    # };
   };
 }
