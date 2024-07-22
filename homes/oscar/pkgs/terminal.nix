97baf6f2fab1a16700ff60a069f99b5664100ba9
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-07-22 21:14:13 +0200

    System switch

--- homes/oscar/pkgs/terminal.nix
+++ homes/oscar/pkgs/terminal.nix
@@ -1,9 +1,14 @@
-{ pkgs, ... }: {
-  programs.alacritty.enable = true;
+{ pkgs, ... }:
+{
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      font.size = "16";
+    };
+  };
   # programs.wezterm.enable = true;
-  home.packages = with pkgs;
-    [
-      # kitty
-      cool-retro-term
-    ];
+  home.packages = with pkgs; [
+    # kitty
+    # cool-retro-term
+  ];
 }
