0c99c7380bad9efe6097cfffecefdca9327517e1
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-08-19 14:50:24 +0200

    System switch

--- pkgs/incus.nix
+++ pkgs/incus.nix
@@ -5,5 +5,19 @@
   };
   users.users.oscar.extraGroups = [ "incus-admin" ];
 
-  networking.nftables.enable = true;
+  networking = {
+    nftables.enable = true;
+
+    # enable instances to get an ipv4 address (to access the internet)
+    firewall.interfaces.incusbr0 = {
+      allowedTCPPorts = [
+        53
+        67
+      ];
+      allowedUDPPorts = [
+        53
+        67
+      ];
+    };
+  };
 }
