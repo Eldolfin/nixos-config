e86bdbae4bf11a1b35b728f9805c2ef5a41c0962
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-08-20 02:44:40 +0200

    add setup-standalone script to setup home manager on any distro?

---
+++ homes/oscar/setup-standalone.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -xe
+
+if command -v nix; then
+	echo Nix is already installed, skipping
+else
+	echo Installing nix using the determinate nix installer https://zero-to-nix.com/start/install
+	curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
+fi
+
+if [ -d ~/nixos-config ]; then
+	echo Eldolfin/nixos-config is already cloned, skipping
+else
+	pushd ~ || exit
+	git clone github.com/Eldolfin/nixos-config
+	popd
+
+	echo Running initial home-manager switch
+	nix run home-manager/master -- switch ~/nixos-config/homes/oscar
+fi
