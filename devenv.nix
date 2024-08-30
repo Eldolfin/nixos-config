28febec3a82270f2e5dde8eb93a95143f03ca89e
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2024-08-31 00:23:59 +0200

    System switch

--- devenv.nix
+++ devenv.nix
@@ -4,7 +4,10 @@
   packages = [ pkgs.git ];
   languages.nix.enable = true;
 
-  pre-commit.hooks.nixfmt.enable = true;
+  pre-commit.hooks.nixfmt = {
+    enable = true;
+    package = pkgs.nixfmt-rfc-style;
+  };
   pre-commit.hooks.shfmt.enable = true;
   pre-commit.hooks.shellcheck.enable = true;
   pre-commit.hooks.typos.enable = true;
