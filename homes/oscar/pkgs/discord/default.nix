60d5a747fba6633f51c6ea4251dd67bc95523927
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2024-08-30 02:19:10 +0200

    System switch

--- homes/oscar/pkgs/discord/default.nix
+++ homes/oscar/pkgs/discord/default.nix
@@ -3,17 +3,20 @@
   # imports = [ inputs.nixcord.homeManagerModules.nixcord ];
   programs.nixcord = {
     enable = true;
-    # frameless = true;
 
-    # discord.enable = false;
+    discord.enable = false;
     vesktop.enable = true;
 
-    plugins = {
-      betterUploadButton.enable = true;
-      # callTimer.enable = true;
-      # clearURLs.enable = true;
-      # dearrow.enable = true;
-      # fakeNitro.enable = true;
+    config = {
+
+      frameless = true;
+      plugins = {
+        betterUploadButton.enable = true;
+        callTimer.enable = true;
+        clearURLs.enable = true;
+        dearrow.enable = true;
+        fakeNitro.enable = true;
+      };
     };
   };
 }
