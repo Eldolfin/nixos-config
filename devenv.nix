2d758ddf2bcfaad39729c0154879facc8b4626da
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2024-09-03 01:48:16 +0200

    remove useless pre commit hooks

--- devenv.nix
+++ devenv.nix
@@ -12,6 +12,4 @@
     package = pkgs.nixfmt-rfc-style;
   };
   pre-commit.hooks.shfmt.enable = true;
-  # pre-commit.hooks.shellcheck.enable = true; # too restrictive
-  pre-commit.hooks.typos.enable = true;
 }
