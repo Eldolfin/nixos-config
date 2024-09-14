4675fbe249070e1b9e2a352f76699ac6e535ed2a
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2024-09-14 12:23:38 +0200

    volume change: add mute

--- homes/oscar/pkgs/i3/default.nix
+++ homes/oscar/pkgs/i3/default.nix
@@ -32,15 +32,24 @@
             set -e
 
             if [ $# != 1 ]; then
-            	echo "Usage: $0 [i|d]"
+            	echo "Usage: ./volume-change.sh [i|d|t]"
+            	echo "i: increase volume"
+            	echo "d: decrease volume"
+            	echo "t: toggle mute"
             	exit 1
             fi
 
             ${pkgs.pamixer}/bin/pamixer -"$1" ${volumeStep}
             VOL=$(${pkgs.pamixer}/bin/pamixer --get-volume)
+            ICON=
+            if [ "$(${pkgs.pamixer}/bin/pamixer --get-mute)" = "true" ]; then
+            	ICON="${pkgs.gnome-themes-extra}/share/icons/HighContrast/256x256/status/audio-volume-muted.png"
+            else
+            	ICON="${pkgs.gnome-themes-extra}/share/icons/HighContrast/256x256/devices/audio-speakers.png"
+            fi
 
             notify-send "Volume: $VOL" \
-              -i ${pkgs.gnome-themes-extra}/share/icons/HighContrast/256x256/devices/audio-speakers.png \
+              -i "$ICON" \
               -h int:value:$VOL \
               -h string:synchronous:volume
 
@@ -62,7 +71,7 @@
           "${mod}+k" = "focus up";
           "${mod}+l" = "focus right";
 
-          "XF86AudioMute" = "exec pamixer -t";
+          "XF86AudioMute" = "exec ${volumeChange}/bin/volume-change t";
           "XF86AudioLowerVolume" = "exec ${volumeChange}/bin/volume-change d";
           "XF86AudioRaiseVolume" = "exec ${volumeChange}/bin/volume-change i";
           "F2" = "exec ${volumeChange}/bin/volume-change d";
