aeb5bf6683f34e45b13ceea8dee2bcf7d69babd1
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2024-09-25 23:05:39 +0200

    change things on i3

--- homes/oscar/pkgs/i3/default.nix
+++ homes/oscar/pkgs/i3/default.nix
@@ -26,36 +26,7 @@
       keybindings =
         let
           mod = config.xsession.windowManager.i3.config.modifier;
-          volumeStep = toString 5;
-          volumeChange = pkgs.writeShellScriptBin "volume-change" ''
-            #!/bin/sh
-            set -e
-
-            if [ $# != 1 ]; then
-            	echo "Usage: ./volume-change.sh [i|d|t]"
-            	echo "i: increase volume"
-            	echo "d: decrease volume"
-            	echo "t: toggle mute"
-            	exit 1
-            fi
-
-            ${pkgs.pamixer}/bin/pamixer -"$1" ${volumeStep}
-            VOL=$(${pkgs.pamixer}/bin/pamixer --get-volume)
-            ICON=
-            if [ "$(${pkgs.pamixer}/bin/pamixer --get-mute)" = "true" ]; then
-            	ICON="${pkgs.gnome-themes-extra}/share/icons/HighContrast/256x256/status/audio-volume-muted.png"
-            else
-            	ICON="${pkgs.gnome-themes-extra}/share/icons/HighContrast/256x256/devices/audio-speakers.png"
-            fi
-
-            ${pkgs.libnotify}/bin/notify-send "Volume: $VOL" \
-              -t 1000 \
-              -i "$ICON" \
-              -h int:value:$VOL \
-              -h string:synchronous:volume
-
-            ${pkgs.pulseaudio}/bin/paplay ~/Music/sounds/audio-volume-change.oga
-          '';
+          volumeChange = import ./changeVolume.nix pkgs;
         in
         lib.mkOptionDefault {
           "${mod}+Return" = "exec alacritty";
