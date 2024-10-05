0f7653df6af0b622987f55e49e36c82e38759668
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2024-10-05 12:43:12 +0200

    gruber darker theme on helix

--- homes/oscar/pkgs/helix/default.nix
+++ homes/oscar/pkgs/helix/default.nix
@@ -7,7 +7,7 @@
     defaultEditor = true;
 
     settings = {
-      theme = "tokyonight";
+      theme = "gruber-darker";
       editor = {
         auto-format = true;
         color-modes = true;
