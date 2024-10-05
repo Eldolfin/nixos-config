d103c3fc4d4a48b6113212dddbea2afa16b901bf
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2024-10-05 16:39:37 +0200

    migrating i3 config to nix

--- homes/oscar/pkgs/i3/default.nix
+++ homes/oscar/pkgs/i3/default.nix
@@ -28,7 +28,7 @@
           mod = config.xsession.windowManager.i3.config.modifier;
           volumeChange = import ./changeVolume.nix pkgs;
         in
-        lib.mkOptionDefault {
+        lib.mkForce {
           "${mod}+Return" = "exec alacritty";
           "${mod}+e" = "exec firefox";
           "${mod}+Shift+e" = "exec firefox --private-window";
@@ -38,11 +38,6 @@
           "${mod}+w" = "exec /home/oscar/.config/i3/scripts/i3-display-swap.sh";
           "${mod}+m" = "exec /home/oscar/bin/scripts/toggle-lamp.sh";
 
-          "${mod}+h" = "focus left";
-          "${mod}+j" = "focus down";
-          "${mod}+k" = "focus up";
-          "${mod}+l" = "focus right";
-
           "XF86AudioMute" = "exec ${volumeChange}/bin/volume-change t";
           "XF86AudioLowerVolume" = "exec ${volumeChange}/bin/volume-change d";
           "XF86AudioRaiseVolume" = "exec ${volumeChange}/bin/volume-change i";
@@ -51,6 +46,39 @@
 
           # sound effects
           "${mod}+Shift+Return" = "exec --no-startup-id ${pkgs.pulseaudio}/bin/paplay Music/sounds/boom.wav";
+
+          "${mod}+h" = "focus left";
+          "${mod}+j" = "focus down";
+          "${mod}+k" = "focus up";
+          "${mod}+l" = "focus right";
+
+          "${mod}+Shift+h" = "move left";
+          "${mod}+Shift+j" = "move down";
+          "${mod}+Shift+k" = "move up";
+          "${mod}+Shift+l" = "move right";
+
+          "${mod}+a" = "layout toggle stacking splith splitv";
+
+          "${mod}+Control+1" = "move container to workspace number 1; workspace number 1";
+          "${mod}+Control+2" = "move container to workspace number 2; workspace number 2";
+          "${mod}+Control+3" = "move container to workspace number 3; workspace number 3";
+          "${mod}+Control+4" = "move container to workspace number 4; workspace number 4";
+          "${mod}+Control+5" = "move container to workspace number 5; workspace number 5";
+          "${mod}+Control+6" = "move container to workspace number 6; workspace number 6";
+          "${mod}+Control+7" = "move container to workspace number 7; workspace number 7";
+          "${mod}+Control+8" = "move container to workspace number 8; workspace number 8";
+          "${mod}+Control+9" = "move container to workspace number 9; workspace number 9";
+          "${mod}+Control+0" = "move container to workspace number 10; workspace number 10";
+
+          "${mod}+Control+h" = "move workspace to output left";
+          "${mod}+Control+j" = "move workspace to output down";
+          "${mod}+Control+k" = "move workspace to output up";
+          "${mod}+Control+l" = "move workspace to output right";
+
+          "${mod}+y" = "resize shrink width 10 px or 10 ppt";
+          "${mod}+u" = "resize grow height 10 px or 10 ppt";
+          "${mod}+i" = "resize shrink height 10 px or 10 ppt";
+          "${mod}+o" = "resize grow width 10 px or 10 ppt";
         };
     };
   };
