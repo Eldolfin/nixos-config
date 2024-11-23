0850669cc3718d518c35e96e06457e8f214a956c
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2024-11-23 16:29:14 +0100

    fix(home-manager): disable html manual to fix build

--- homes/oscar/home.nix
+++ homes/oscar/home.nix
@@ -26,20 +26,11 @@
     username = "oscar";
     homeDirectory = "/home/oscar";
   };
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "22.11";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-  # offline home manager manual :)
-  manual.html.enable = true;
+  # offline home manager manual :) (broken atm)
+  manual.html.enable = false;
 
   #   stylix.enable = true;
   #   stylix.autoEnable = true;
@@ -49,4 +40,14 @@
     "text/html" = "firefox.desktop";
     "x-scheme-handler/http" = "firefox.desktop";
   };
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.11";
 }
