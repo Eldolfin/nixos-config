55f6882a6516c7c79e68e74cfbb58c0433b6afe6
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2024-11-24 15:27:18 +0100

    fix(login-screen-test): wait for username to be shown before taking the screensho

--- tests/login-screen.nix
+++ tests/login-screen.nix
@@ -4,8 +4,9 @@
     services.displayManager.autoLogin.enable = false;
   };
   testScript = ''
+    start_all()
     c.wait_for_unit("graphical.target")
-    c.sleep(10)
+    c.wait_for_text("Oscar Le Dauphin")
     c.screenshot("${name}")
   '';
 }
