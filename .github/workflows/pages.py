4170ce292fc666afbb8adf55552bae2d62fb4c8d
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2024-11-24 19:01:18 +0100

    fix(ci): correct markdown for jekyll

--- .github/workflows/pages.py
+++ .github/workflows/pages.py
@@ -2,13 +2,23 @@ import os
 import subprocess as sp
 import datetime
 import logging
+from typing import Union, Tuple, List, Dict
 
 OUTPUT_FILE = "README.md"
 
 
 def main():
     commits = os.listdir()
-    commits = list(
+    commits = filter_commit_dir(commits)
+    commits, commits_dates = get_commit_dates(commits)
+
+    markdown = generate_markdown(commits, commits_dates)
+    with open(OUTPUT_FILE, "w") as f:
+        f.write(markdown)
+
+
+def filter_commit_dir(commits: List[str]) -> List[str]:
+    return list(
         filter(
             lambda entry: os.path.isdir(entry)
             and not entry.startswith(".")
@@ -17,64 +27,81 @@ def main():
         )
     )
 
+
+def get_commit_date(commit: str) -> Union[datetime.datetime, False]:
+    cmd = sp.run(
+        [
+            "git",
+            "show",
+            "-q",
+            "--pretty=format:'%ct'",
+            commit,
+            "--",
+        ],
+        capture_output=True,
+        text=True,
+    )
+    timestamp: str = cmd.stdout
+    timestamp = timestamp.strip("'")
+    try:
+        timestamp: float = float(timestamp)
+    except ValueError:
+        logging.warn(f"Failed to get commit date for commit {commit}. Skipping it...")
+        return False
+    parsed_date = datetime.datetime.fromtimestamp(timestamp)
+    return parsed_date
+
+
+def get_commit_dates(
+    commits: List[str],
+) -> Tuple[List[str], Dict[str, datetime.datetime]]:
     commits_dates = {}
 
     for commit in commits:
-        cmd = sp.run(
-            [
-                "git",
-                "show",
-                "-q",
-                "--pretty=format:'%ct'",
-                commit,
-                "--",
-            ],
-            capture_output=True,
-            text=True,
-        )
-        timestamp: str = cmd.stdout
-        timestamp = timestamp.strip("'")
-        try:
-            timestamp: float = float(timestamp)
-        except ValueError:
-            logging.warn(
-                f"Failed to get commit date for commit {commit}. Skipping it..."
-            )
+        date = get_commit_date(commit)
+        if not date:
             continue
-        parsed_date = datetime.datetime.fromtimestamp(timestamp)
-        commits_dates[commit] = parsed_date
+        commits_dates[commit] = date
 
     commits = list(filter(lambda c: c in commits_dates, commits))
     commits.sort(key=lambda c: commits_dates[c], reverse=True)
 
-    with open(OUTPUT_FILE, "w") as f:
-        f.write("# List of screenshots from the integration tests\n")
-        for commit in commits:
-            f.write(f"## {commits_dates[commit]}\n")
-            f.write(
-                f"commit: [{commit}](https://github.com/Eldolfin/nixos-config/commit/{commit})\n"
-            )
-            screenshots = list(os.listdir(commit))
+    return commits, commits_dates
 
-            # Header
-            f.write("|")
-            for screenshot in screenshots:
-                name = screenshot.removesuffix(".png").replace("_", "-")
-                f.write(
-                    f"[{name}](https://github.com/Eldolfin/nixos-config/blob/{commit}/tests/{name}.nix)|"
-                )
-            f.write("\n")
 
-            f.write("|" + "-|" * len(screenshots) + "\n")
+def generate_markdown(
+    commits: List[str], commits_dates: Dict[str, datetime.datetime]
+) -> str:
+    title = "# List of screenshots from the integration tests\n"
+    contents = "\n".join(
+        map(lambda c: commit_to_markdown(commits_dates[c], c), commits)
+    )
+    markdown = "\n".join((title, contents))
 
-            # Images
-            f.write("|")
-            for screenshot in screenshots:
-                f.write(
-                    f"[![]({commit}/{screenshot})](https://eldolfin.github.io/nixos-config/{commit}/{screenshot})"
-                )
-                f.write("|")
-            f.write("\n")
+    return markdown
+
+
+def commit_to_markdown(commit_date: datetime.datetime, commit: str) -> str:
+    screenshots = list(os.listdir(commit))
+
+    def to_clickable_link(screenshot: str) -> str:
+        name = screenshot.removesuffix(".png").replace("_", "-")
+        return f"[{name}](https://github.com/Eldolfin/nixos-config/blob/{commit}/tests/{name}.nix)"
+
+    def to_clickable_image(screenshot: str) -> str:
+        return f"[![]({commit}/{screenshot})](https://eldolfin.github.io/nixos-config/{commit}/{screenshot})"
+
+    header = "|".join(map(to_clickable_link, screenshots))
+    separator = "|".join("-" * len(screenshots))
+    body = "|".join(map(to_clickable_image, screenshots))
+
+    table = "\n" + "\n".join((header, separator, body)) + "\n"
+    title = (
+        f"## [{commit_date}](https://github.com/Eldolfin/nixos-config/commit/{commit})"
+    )
+
+    row = "\n".join((title, table)) + "\n"
+    return row
 
 
 if __name__ == "__main__":
