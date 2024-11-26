eb939b6681bca7c714a3f0c32f1bc48652471735
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2024-11-26 21:37:18 +0100

    feat(helix): add scls for all languages

---
+++ homes/oscar/pkgs/helix/scls.nix
@@ -0,0 +1,72 @@
+{
+  pkgs,
+  scls,
+  lib,
+  ...
+}: let
+  tomlFormat = pkgs.formats.toml {};
+  sclsPkg = scls.defaultPackage.x86_64-linux;
+in {
+  home.packages = [sclsPkg];
+
+  programs.helix.languages. language-server . scls.command = lib.getExe sclsPkg;
+
+  xdg.configFile."helix/external-snippets.toml".source = tomlFormat.generate "scls-snippets" {
+    sources = [
+      {
+        name = "friendly-snippets";
+        git = "https://github.com/rafamadriz/friendly-snippets.git";
+        paths = [
+          {
+            scope = ["python"];
+            path = "snippets/python/";
+          }
+          {
+            scope = ["rust"];
+            path = "snippets/rust";
+          }
+          {
+            scope = ["nix"];
+            path = "snippets/nix.json";
+          }
+          {
+            scope = ["go"];
+            path = "snippets/go.json";
+          }
+          {
+            scope = ["bash"];
+            path = "snippets/shell";
+          }
+          {
+            scope = ["vue"];
+            path = "snippets/frameworks/vue";
+          }
+          {
+            scope = ["c"];
+            path = "snippets/c";
+          }
+          {
+            scope = ["cpp"];
+            path = "snippets/cpp";
+          }
+          {
+            scope = ["dockerfile"];
+            path = "snippets/docker/docker_file.json";
+          }
+          {
+            scope = ["docker-compose"];
+            path = "snippets/docker/docker-compose.json";
+          }
+          {
+            scope = ["java"];
+            path = "snippets/java";
+          }
+          {
+            scope = ["python" "rust" "go" "vue" "c" "cpp" "java"];
+            path = "snippets/loremipsum.json";
+          }
+        ];
+      }
+    ];
+  };
+}
