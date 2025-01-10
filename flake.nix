c01736ba14d524fc8d620cba5eac9f5d5dae6639
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2025-01-10 21:57:26 +0100

    feat(wol-agent): add wol-agent not ready

--- flake.nix
+++ flake.nix
@@ -40,6 +40,11 @@
       url = "github:estin/simple-completion-language-server/main";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    wol-api = {
+      url = "git+ssh://git@github.com/eldolfin/wol-api.git?ref=feat/open-any-application&dir=wol-api";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
@@ -52,6 +57,7 @@
     helix,
     nixcord,
     nixpkgs-master,
+    wol-api,
     ...
   } @ inputs: let
     system = "x86_64-linux";
@@ -140,6 +146,7 @@
           modules =
             nixosModules
             ++ [
+              wol-api.nixosModules.default
               ./hosts/tour/configuration.nix
               ./hosts/tour/hardware-configuration.nix
               {home-manager.extraSpecialArgs = specialArgs;}
---
+++ pkgs/wol-agent.nix
@@ -0,0 +1,6 @@
+_: {
+  eldolfin.services.wol-agent = {
+    enable = true;
+    domain = "192.168.1.1:3001";
+  };
+}
