3848ec8dcfbd142401e58ea8959f0b012dd20e28
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2025-01-19 18:07:10 +0100

    fix(oracle-server): build

--- hosts/oracle-x86/configuration.nix
+++ hosts/oracle-x86/configuration.nix
@@ -5,19 +5,21 @@
   ];
 
   eldolfin.services.wol-agent.machine-name = "oracle";
-  networking.wireguard.interfaces = {
-    freebox = {
-      ips = ["192.168.27.90/32"];
-      mtu = 1360;
-      privateKeyFile = "/run/secrets/wireguard/oracle/privateKey";
-      peers = [
-        {
-          allowedIPs = ["192.168.27.64/27" "192.168.1.0/24"];
-          endpoint = "88.181.110.252:2802";
-          publicKey = "KS+dja7K1VsBpfNsIn2kBJln/PPrCfv7hbHluF12EWw="; # pragma: allowlist secret
-          presharedKeyFile = "/run/secrets/wireguard/oracle/presharedKey";
-        }
-      ];
+  networking = {
+    wireguard.interfaces = {
+      freebox = {
+        ips = ["192.168.27.90/32"];
+        mtu = 1360;
+        privateKeyFile = "/run/secrets/wireguard/oracle/privateKey";
+        peers = [
+          {
+            allowedIPs = ["192.168.27.64/27" "192.168.1.0/24"];
+            endpoint = "88.181.110.252:2802";
+            publicKey = "KS+dja7K1VsBpfNsIn2kBJln/PPrCfv7hbHluF12EWw="; # pragma: allowlist secret
+            presharedKeyFile = "/run/secrets/wireguard/oracle/presharedKey";
+          }
+        ];
+      };
     };
     hostName = "instance-20250108-2030";
     domain = "";
