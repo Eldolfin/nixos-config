2e69483de3575c40d3cb494d945cd1e63b6816a8
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2025-02-20 15:04:52 +0100

    feat(cachix-subsitutuers): add cuda-maintainers

--- pkgs/cachix-subsituter.nix
+++ pkgs/cachix-subsituter.nix
@@ -7,10 +7,12 @@
       trusted-substituters = [
         "https://cache.nixos.org"
         "https://nixos-eldolfin.cachix.org"
+        "https://cuda-maintainers.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nixos-eldolfin.cachix.org-1:+9moa8pYw+2ie0kWZWjhhNu1Axa+G/2ssdSClvfE/7w="
+        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       ];
     };
   };
--- pkgs/open-webui.nix
+++ pkgs/open-webui.nix
@@ -5,7 +5,7 @@
     ollama = {
       enable = true;
       loadModels = ["deepseek-r1:1.5b" "deepseek-r1:8b"];
-      acceleration = "cuda";
+      # acceleration = "cuda"; # (by default)
     };
     open-webui = {
       enable = true;
