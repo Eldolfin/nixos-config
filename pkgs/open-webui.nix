e98748c9cdd7046eca1baf0b255bbe68331cfa62
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2025-02-20 15:22:33 +0100

    fix(open-webui): re enable cuda acceleration

--- pkgs/open-webui.nix
+++ pkgs/open-webui.nix
@@ -5,7 +5,7 @@
     ollama = {
       enable = true;
       loadModels = ["deepseek-r1:1.5b" "deepseek-r1:8b"];
-      # acceleration = "cuda"; # (by default)
+      acceleration = "cuda";
     };
     open-webui = {
       enable = true;
