81344a30fda8912e27fe52412e7bad00e998574c
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2025-02-27 21:54:58 +0100

    fix(outline): add dex oidc service

--- pkgs/outline.nix
+++ pkgs/outline.nix
@@ -1,42 +1,57 @@
 {pkgs, ...}: let
-  localPort = 8082;
-  baseUrl = "outline.internal.eldolfin.top";
+  # local only port
+  outlinePort = 8082;
+  outlineUrl = "outline.internal.eldolfin.top";
+  dexPort = 8083;
+  dexUrl = "dex.internal.eldolfin.top";
+  dexOutlineSecretFile = "${pkgs.writeText "outline-oidc-secret" "test123"}";
 in {
+  # https://wiki.nixos.org/wiki/Outline
   services = {
     outline = {
       enable = true;
-      publicUrl = "https://${baseUrl}";
-      port = localPort;
+      publicUrl = "https://${outlineUrl}";
+      port = outlinePort;
       storage = {
         storageType = "local";
       };
+      oidcAuthentication = {
+        # Parts taken from
+        # http://dex.localhost/.well-known/openid-configuration
+        authUrl = "https://${dexUrl}/auth";
+        tokenUrl = "http://${dexUrl}/token";
+        userinfoUrl = "http://${dexUrl}/userinfo";
+        clientId = "outline";
+        clientSecretFile = dexOutlineSecretFile;
+        scopes = ["openid" "email" "profile"];
+        usernameClaim = "preferred_username";
+        displayName = "Dex";
+      };
     };
 
-    # TODO: configure OIDC
-    # https://wiki.nixos.org/wiki/Outline
     dex = {
       enable = true;
       settings = {
-        issuer = "http://dex.localhost";
+        issuer = "https://${dexUrl}";
         storage.type = "sqlite3";
-        web.http = "127.0.0.1:5556";
+        web.http = "127.0.0.1:${toString dexPort}";
         enablePasswordDB = true;
         staticClients = [
           {
             id = "outline";
             name = "Outline Client";
-            redirectURIs = ["http://localhost:3000/auth/oidc.callback"];
-            secretFile = "${pkgs.writeText "outline-oidc-secret" "test123"}";
+            redirectURIs = ["https://${outlineUrl}/auth/oidc.callback"];
+            secretFile = dexOutlineSecretFile;
           }
         ];
         staticPasswords = [
           {
-            email = "user.email@example.com";
+            email = "oscar@eldolfin.top";
             # bcrypt hash of the string "password": $(echo password | htpasswd -BinC 10 admin | cut -d: -f2)
-            hash = "10$TDh68T5XUK10$TDh68T5XUK10$TDh68T5XUK";
-            username = "test";
+            hash = "$2y$10$y9q6O0yEQ1xD6XWjT9ifAe6zvIfscpuzenQQAH5k1HQYthrKSOJsm";
+            username = "oscar";
             # easily generated with `$ uuidgen`
-            userID = "6D196B03-8A28-4D6E-B849-9298168CBA34";
+            userID = "e447cb79-56ed-42b1-8d04-1c9e453b2d81";
           }
         ];
       };
@@ -44,15 +59,16 @@ in {
     caddy = {
       enable = true;
       virtualHosts = {
-        "https://${baseUrl}" = {
+        "https://${outlineUrl}" = {
           extraConfig = ''
             tls internal
-            reverse_proxy localhost:${toString localPort}
+            reverse_proxy localhost:${toString outlinePort}
           '';
         };
-        "http://${baseUrl}" = {
+        "https://${dexUrl}" = {
           extraConfig = ''
-            reverse_proxy localhost:${toString localPort}
+            tls internal
+            reverse_proxy localhost:${toString dexPort}
           '';
         };
       };
