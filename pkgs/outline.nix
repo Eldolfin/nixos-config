b45352cde088ad905383a3c9e4512d53f530211d
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2025-02-27 16:47:49 +0100

    wip(outline): add external oidc

--- pkgs/outline.nix
+++ pkgs/outline.nix
@@ -1,4 +1,4 @@
-let
+{pkgs, ...}: let
   localPort = 8082;
   baseUrl = "outline.internal.eldolfin.top";
 in {
@@ -9,6 +9,36 @@ in {
       port = localPort;
       storage = {
         storageType = "local";
+      };
+    };
+
+    # TODO: configure OIDC
+    # https://wiki.nixos.org/wiki/Outline
+    dex = {
+      enable = true;
+      settings = {
+        issuer = "http://dex.localhost";
+        storage.type = "sqlite3";
+        web.http = "127.0.0.1:5556";
+        enablePasswordDB = true;
+        staticClients = [
+          {
+            id = "outline";
+            name = "Outline Client";
+            redirectURIs = ["http://localhost:3000/auth/oidc.callback"];
+            secretFile = "${pkgs.writeText "outline-oidc-secret" "test123"}";
+          }
+        ];
+        staticPasswords = [
+          {
+            email = "user.email@example.com";
+            # bcrypt hash of the string "password": $(echo password | htpasswd -BinC 10 admin | cut -d: -f2)
+            hash = "10$TDh68T5XUK10$TDh68T5XUK10$TDh68T5XUK";
+            username = "test";
+            # easily generated with `$ uuidgen`
+            userID = "6D196B03-8A28-4D6E-B849-9298168CBA34";
+          }
+        ];
       };
     };
     caddy = {
