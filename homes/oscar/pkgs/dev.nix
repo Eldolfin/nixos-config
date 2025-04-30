58ba20a85081bdcd7e8d2cb5b245f0fdfd6fa5fa
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2025-05-01 00:19:42 +0200

    feat(dev): remove a bit of bloat

--- homes/oscar/pkgs/dev.nix
+++ homes/oscar/pkgs/dev.nix
@@ -3,146 +3,30 @@
   isTour,
   ...
 }: {
-  programs.bun.enable = true;
   programs.gh.enable = true;
   home.packages = with pkgs; (
     [
-      # vscode-fhs
-      # jetbrains
-      # jetbrains-toolbox
-
       # docker
       docker-compose
-      # dive
+      dive
 
-      # java
-      # jetbrains.idea-community
-      jetbrains.idea-ultimate
-      # jetbrains.pycharm-professional
-      # maven
-      openjdk17
-
-      # rust
-      # evcxr
-      # rust-analyzer
-      # rustup
-      # bacon
       rustc
       cargo
       clippy
 
-      # for leptos
-      # trunk
-      # leptosfmt
-      # cargo-leptos
-      # cargo-generate
-      # sass # for css
-
       # c/c++
-      # jetbrains.clion
-      # valgrind
-      # gcc12
       clang-tools
-      # clang
       gcc
-      # criterion
-      # gtest
-      # gcovr
-      # boost
-      # ninja
       gdb
 
-      # go
-      # go
-      # gopls
-      # gotools
-
-      # python
-      # jetbrains.pycharm-professional
-      # jupyter-all
-      # mypy
-      # poetry
-      # poethepoet
-      # black
-      # conda
-
-      # (python311Full.withPackages (ppkgs: [
-      # ppkgs.scipy
-      # ppkgs.matplotlib
-      # ppkgs.notebook
-      # ppkgs.numpy
-      # ppkgs.pytest
-      # ppkgs.psutil
-      # ]))
-      python3Full
+      python3
       python3Packages.pip
-      # python311Packages.bpython
-      # grip # github markdown preview
 
-      # c#
-      # mono
-      # jetbrains.rider
-      # dotnet-sdk_7
-      # omnisharp-roslyn
-
-      # js
-      # yarn
       nodejs
       typescript
-      # bun
-      # deno
-
-      # nix
-      # nixpkgs-fmt
-      # nil
-
-      # kubernetes
-      # kubectl
-      # minikube
-
-      # android
-      # android-studio
-      # android-tools # adb, fastboot etc. enabled in common.nix?
-
-      # Haskell
-      # ghc
-
-      # ocaml
-      # ocamlPackages.ocaml-lsp
-      # ocamlPackages.ocamlformat
-
-      # Common lisp
-      # sbcl
-
-      # sql
-      # sqlfluff
-      # dbeaver
-
-      # zig
-      # zig
-      # zls
-
-      # php
-      # php83Packages.composer
-      # php83
-
-      # lua?
-      # luajitPackages.luarocks
-
-      # Language servers
-      # now in helix config directly
-      # lua-language-server
-      # nodePackages_latest.typescript-language-server
-      # docker-compose-language-service
-      # yaml-language-server
-      # dockerfile-language-server-nodejs
-      # python311Packages.python-lsp-server
-
-      # Arduino
-      # arduino
+      pnpm
 
       # misc
-      # graphviz
       nodePackages.live-server
       sshfs
       entr
@@ -150,10 +34,6 @@
       tea
       yazi
       just
-      # godot_4
-      # dig
-      # mold
-      # act # github action runner
       sd
     ]
     ++ lib.optional isTour nvtopPackages.nvidia
