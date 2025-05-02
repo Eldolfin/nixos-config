5657d007f77e6b55701daa01c5221acee35022ef
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2025-05-02 18:04:08 +0200

    fix(wol/build): remove vdi support which was broken anyways

--- pkgs/wol-agent.nix
+++ pkgs/wol-agent.nix
@@ -2,20 +2,8 @@
   eldolfin.services.wol-agent = {
     enable = true;
     domain = "ws://192.168.1.1:3030";
-    # domain = "ws://192.168.1.167:3030";
   };
   networking = {
     interfaces.eno1.wakeOnLan.enable = true;
-    # VDI
-    firewall.allowedTCPPorts = [
-      1122 # webtransport vdi
-      1123 # websocket vdi
-      5173 # dev
-      3000 # dev wol-panel
-      3030 # dev wol-api
-    ];
-    firewall.allowedUDPPorts = [
-      1122 # webtransport vdi
-    ];
   };
 }
