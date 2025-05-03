85914045652fd39c6c993df2b7d4cf7c517e09b0
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2025-05-03 17:39:37 +0200

    fix(ci/montage): use normal font

--- .github/workflows/screenshot-montage.sh
+++ .github/workflows/screenshot-montage.sh
@@ -2,9 +2,9 @@
 
 sha=$(readlink latest | head -c10)
 
-magick montage latest/*.png -font NotoSansMono -tile 3x -pointsize 48 -set label '%f' -frame 5 -shadow -geometry +20+20 montage_raw.png
+magick montage latest/*.png -font Noto-Mono -tile 3x -pointsize 48 -set label '%f' -frame 5 -shadow -geometry +20+20 montage_raw.png
 
 magick montage_raw.png \
 	-resize 1920x1920\> \
-	-font NotoSansMono -gravity North -fill white -stroke black -strokewidth 2 -pointsize 64 -annotate +0+040 "commit: $sha" \
+	-font Noto-Mono -gravity North -fill white -stroke black -strokewidth 2 -pointsize 64 -annotate +0+040 "commit: $sha" \
 	latest.png
