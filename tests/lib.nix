1f7c8df990c687e3e68b5657d09f4963772fb37b
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2025-05-03 21:05:45 +0200

    feat(tests): Update test node configuration

--- tests/lib.nix
+++ tests/lib.nix
@@ -9,27 +9,28 @@ test: {
 in
   (
     nixos-lib.runTest {
+      inherit (test) name testScript;
       hostPkgs = pkgs;
       # This speeds up the evaluation by skipping evaluating documentation
       defaults.documentation.enable = lib.mkDefault false;
       node.specialArgs = specialArgs;
-      imports = [test];
       enableOCR = true;
       nodes = {
-        c = _: {
-          imports = commonModules;
-          virtualisation = {
-            memorySize = 4096;
-            diskSize = 8192;
-            cores = 4;
-            resolution = {
-              x = 1920;
-              y = 1080;
+        c = {pkgs, ...}: (
+          {
+            imports = commonModules;
+            virtualisation = {
+              memorySize = 4096;
+              diskSize = 8192;
+              cores = 4;
+              resolution = {
+                x = 1920;
+                y = 1080;
+              };
             };
-          };
-          # avoid emote welcome screen (it crashes because the file format in incorrect)
-          home-manager.users.oscar.home.file.".local/share/Emote/user_data".text = "";
-        };
+          }
+          // (test.nodes.c {inherit pkgs;})
+        );
       };
     }
   )
