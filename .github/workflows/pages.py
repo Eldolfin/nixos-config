64f757ee0bc623b4b88ac0ace4e6da2fc87a1f3b
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2025-05-03 15:45:05 +0200

    feat(workflows/pages): Add latest symlink to latest commit

--- .github/workflows/pages.py
+++ .github/workflows/pages.py
@@ -5,6 +5,7 @@ import logging
 from typing import Union, Tuple, List, Dict, Literal
 
 OUTPUT_FILE = "README.md"
+LATEST_PATH = "latest"
 
 
 def main():
@@ -15,6 +16,11 @@ def main():
     markdown = generate_markdown(commits, commits_dates)
     with open(OUTPUT_FILE, "w") as f:
         f.write(markdown)
+
+    # create latest symlink to latest commit dir
+    if os.path.exists(LATEST_PATH):
+        os.remove(LATEST_PATH)
+    os.symlink(commits[0], LATEST_PATH)
 
 
 def filter_commit_dir(commits: List[str]) -> List[str]:
@@ -82,7 +88,7 @@ def generate_markdown(
 
 
 def commit_to_markdown(commit_date: datetime.datetime, commit: str) -> str:
-    screenshots = list(os.listdir(commit))
+    screenshots = list(sorted(os.listdir(commit)))
 
     def to_clickable_link(screenshot: str) -> str:
         name = screenshot.removesuffix(".png").replace("_", "-")
