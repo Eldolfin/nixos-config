1517dd5890c3b188d9b1c80ea4146f7f5f2061bd
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2025-05-19 19:41:49 +0200

    feat(ai-commit.sh): change prompt for better commit message generation

--- custom-pkgs/systemswitch/src/ai-commit.sh
+++ custom-pkgs/systemswitch/src/ai-commit.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -xe
 
+PROMPT="
+- Given the git diff and the last few commits, propose a commit message following the project commit format.
+- Output only the commit message nothing more.
+- Do not describe the commit message afterward
+- Do not generate commit messages longer than 80 characters
+- Do not put quotes or stars around the commit message
+"
+
 TMP_LAST_COMMITS='/tmp/aichat-last-commits.txt'
 TMP_GIT_DIFF='/tmp/aichat-git-diff.txt'
 TMP_GITCOMMIT='/tmp/aichat-gitcommit.txt'
@@ -10,7 +18,7 @@ git diff --no-ext-diff --staged >$TMP_GIT_DIFF
 aichat \
 	--model mistral:open-mistral-nemo \
 	--file $TMP_GIT_DIFF --file $TMP_LAST_COMMITS \
-	"Given the git diff and the last few commits, propose a commit message following the project commit format. Output only the commit message nothing more." |
+	"$PROMPT" |
 	tr -d \" >$TMP_GITCOMMIT
 
 git commit -F $TMP_GITCOMMIT --edit
