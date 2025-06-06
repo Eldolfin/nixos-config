0068958d91df0866d47214e55bb855690baad30c
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2025-06-06 17:39:58 +0200

    feat(direnv): Disable silent logging

--- homes/oscar/pkgs/direnv.nix
+++ homes/oscar/pkgs/direnv.nix
@@ -2,12 +2,11 @@
   programs = {
     direnv = {
       enable = true;
-      silent = true;
       nix-direnv.enable = true;
     };
   };
   home.sessionVariables = {
-    # disable direnv is taking a while to execute. Use CTRL-C to give up.
+    # disable "direnv is taking a while to execute. Use CTRL-C to give up."
     DIRENV_WARN_TIMEOUT = 0;
   };
 }
