5e096cd987ecb1b49803e176d80555000f447224
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2025-07-10 20:07:48 +0200

    fix(xdg): remove custom extra portal config?

--- homes/oscar/pkgs/xdg.nix
+++ homes/oscar/pkgs/xdg.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{
   xdg = {
     systemDirs.config = [
       "/etc/xdg"
@@ -8,20 +8,6 @@
       "application/pdf" = "firefox.desktop";
       "text/html" = "firefox.desktop";
       "x-scheme-handler/http" = "firefox.desktop";
-    };
-    portal = {
-      # this is not good but idk what to put here
-      config.common.default = "*";
-      enable = true;
-      # from https://github.com/YaLTeR/niri/wiki/Important-Software
-      extraPortals = with pkgs; [
-        # implements most of the basic functionality, this is the "default fallback portal".
-        xdg-desktop-portal-gtk
-        # required for screencasting support
-        xdg-desktop-portal-gnome
-        # implements the Secret portal, required for certain apps to work
-        gnome-keyring
-      ];
     };
   };
 }
