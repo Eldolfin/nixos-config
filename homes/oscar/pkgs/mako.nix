1adace32eb8771c160b07bcb2bccf700a065f168
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2025-07-18 22:37:20 +0200

    feat(mako): increase max size by a lot

--- homes/oscar/pkgs/mako.nix
+++ homes/oscar/pkgs/mako.nix
@@ -5,6 +5,8 @@
       default-timeout = 10000;
       on-notify = "exec mpv ~/Music/sounds/bell.mp3";
       layer = "overlay"; # show above fullscreen apps
+      width = 400;
+      height = 400;
     };
   };
 }
